92f721d86c806afcb0b81b533456b7055a990571
Sanket Tajane <sanket.tajane@gmail.com>
2017-03-07 22:50:32 -0600

    feat(add-photo): Add photo to user profile

--- src/features/side-users-view/side-users.controller.js
+++ src/features/side-users-view/side-users.controller.js
@@ -2,16 +2,26 @@ angular
     .module('app.sideUser')
     .controller('SideUsersCtrl', SideUsersCtrl);
 
-function SideUsersCtrl(UserInfo, $scope, $ionicPopup, rootRef, Auth, ionicToast) {
+function SideUsersCtrl(UserInfo,
+                       $scope,
+                       $ionicPopup,
+                       $ionicLoading,
+                       rootRef,
+                       Auth,
+                       ionicToast,
+                       $ionicActionSheet,
+                       $cordovaCamera) {
     var vm = this;
     vm.userInfo = {
-        displayName: UserInfo[0].$value,
-        email: UserInfo[1].$value,
+        displayName: _.get(UserInfo, ['0', '$value']),
+        email: _.get(UserInfo, ['1', '$value']),
+        image: _.get(UserInfo, ['2', '$value']),
         password: '******'
     };
     vm.showNamePopup = showNamePopup;
     vm.showEmailPopup = showEmailPopup;
     vm.showPasswordPopup = showPasswordPopup;
+    vm.addMedia = addMedia;
 
     function showNamePopup() {
         $scope.data = {
@@ -126,5 +136,58 @@ function SideUsersCtrl(UserInfo, $scope, $ionicPopup, rootRef, Auth, ionicToast)
                 });
             }
         });
+    }
+
+    function addMedia() {
+        $scope.hideSheet = $ionicActionSheet.show({
+            buttons: [
+                { text: 'Camera' },
+                { text: 'Gallery' }
+            ],
+            titleText: 'Add Photo',
+            cancelText: 'Cancel',
+            buttonClicked: function(index) {
+                addImage(index);
+            }
+        });
+    }
+
+    function addImage(type) {
+        $scope.hideSheet();
+        var options = optionsForType(type);
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            $ionicLoading.show({
+                template: '<ion-spinner icon="spiral"></ion-spinner>'
+            });
+            rootRef.child('users').child(Auth.$getAuth().uid).child('userInfo').child('image').set(imageData).then(function(){
+                vm.userInfo.image = UserInfo[2].$value;
+                $ionicLoading.hide();
+            });
+        }, function(err) {
+            ionicToast.show(err.message, 'bottom', false, 2500, 'rgb(255, 0, 0)');
+        });
+    }
+
+    function optionsForType(type) {
+        var source;
+        switch (type) {
+            case 0:
+                source = Camera.PictureSourceType.CAMERA;
+                break;
+            case 1:
+                source = Camera.PictureSourceType.PHOTOLIBRARY;
+                break;
+        }
+        return {
+            quality : 75,
+            targetWidth: 100,
+            targetHeight: 100,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: source,
+            allowEdit: true,
+            encodingType: Camera.EncodingType.JPEG,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false
+        };
     }
 }
